0a7dc1f83d6a527abe3a69e8bd0c999052b9d837
Ryan Leahy <rleahy@webassign.net>
2014-09-30 12:32:57 -0400

    added logging to events

--- SimpleWebRTC/server.js
+++ SimpleWebRTC/server.js
@@ -3,6 +3,7 @@ var fs = require('fs'),
     express = require('express'),
     https = require('https'),
     http = require('http');
+
 process.on('uncaughtException', function (err) {
     logger.error('uncaughtException', err, err.stack);
 });
@@ -13,9 +14,24 @@ var privateKey = fs.readFileSync('fakekeys/localhost.key').toString(),
 
 var app = express();
 
+app.use(express.errorHandler({dumbExceptions: true, showStack: true}));
+// We need to allow CORS but I'm hungry
+// Also you need to hit https://localhost:8000 bubbe
+
 app.use(express.static(__dirname + '/public'));
 
-https.createServer({key: privateKey, cert: certificate}, app).listen(8000);
-http.createServer(app).listen(8001);
+https.createServer({key: privateKey, cert: certificate}, app).listen(8000)
+    .on('listening', function() {
+        console.log('now listening');
+    })
+    .on('connection', function(res) {
+        console.log('connection established');
+        console.log(res);
+    })
+    .on('error', function() {
+        console.log('error occured');
+    });
+// http.createServer(app).listen(8001);
+
 
 console.log('running on https://localhost:8000 and http://localhost:8001');
